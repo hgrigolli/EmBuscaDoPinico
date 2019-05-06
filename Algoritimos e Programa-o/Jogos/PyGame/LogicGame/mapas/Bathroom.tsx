e53150969cbffee46d4f9cae33a0a56e32003ed7
hgrigolli <34338980+hgrigolli@users.noreply.github.com>
2019-05-06 06:39:37 -0300

    Add files via upload

---
+++ Algoritimos e Programa-o/Jogos/PyGame/LogicGame/mapas/Bathroom.tsx
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Bathroom" tilewidth="97" tileheight="134" tilecount="37" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="17" height="24" source="../imagens/bath/bath_01.png"/>
+ </tile>
+ <tile id="1">
+  <image width="30" height="27" source="../imagens/bath/bath_02.png"/>
+ </tile>
+ <tile id="2">
+  <image width="46" height="134" source="../imagens/bath/bath_03.png"/>
+ </tile>
+ <tile id="3">
+  <image width="48" height="133" source="../imagens/bath/bath_04.png"/>
+ </tile>
+ <tile id="4">
+  <image width="48" height="133" source="../imagens/bath/bath_05.png"/>
+ </tile>
+ <tile id="5">
+  <image width="48" height="133" source="../imagens/bath/bath_06.png"/>
+ </tile>
+ <tile id="6">
+  <image width="48" height="133" source="../imagens/bath/bath_07.png"/>
+ </tile>
+ <tile id="7">
+  <image width="96" height="48" source="../imagens/bath/bath_08.png"/>
+ </tile>
+ <tile id="8">
+  <image width="46" height="96" source="../imagens/bath/bath_09.png"/>
+ </tile>
+ <tile id="9">
+  <image width="96" height="49" source="../imagens/bath/bath_10.png"/>
+ </tile>
+ <tile id="10">
+  <image width="96" height="133" source="../imagens/bath/bath_11.png"/>
+ </tile>
+ <tile id="11">
+  <image width="97" height="120" source="../imagens/bath/bath_12.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="54" source="../imagens/bath/bath_13.png"/>
+ </tile>
+ <tile id="13">
+  <image width="48" height="120" source="../imagens/bath/bath_14.png"/>
+ </tile>
+ <tile id="14">
+  <image width="48" height="120" source="../imagens/bath/bath_15.png"/>
+ </tile>
+ <tile id="15">
+  <image width="48" height="120" source="../imagens/bath/bath_16.png"/>
+ </tile>
+ <tile id="16">
+  <image width="47" height="48" source="../imagens/bath/bath_17.png"/>
+ </tile>
+ <tile id="17">
+  <image width="45" height="72" source="../imagens/bath/bath_18.png"/>
+ </tile>
+ <tile id="18">
+  <image width="48" height="53" source="../imagens/bath/bath_19.png"/>
+ </tile>
+ <tile id="19">
+  <image width="48" height="53" source="../imagens/bath/bath_20.png"/>
+ </tile>
+ <tile id="20">
+  <image width="30" height="70" source="../imagens/bath/bath_21.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="69" source="../imagens/bath/bath_22.png"/>
+ </tile>
+ <tile id="22">
+  <image width="48" height="53" source="../imagens/bath/bath_23.png"/>
+ </tile>
+ <tile id="23">
+  <image width="49" height="53" source="../imagens/bath/bath_24.png"/>
+ </tile>
+ <tile id="24">
+  <image width="12" height="26" source="../imagens/bath/bath_25.png"/>
+ </tile>
+ <tile id="25">
+  <image width="47" height="43" source="../imagens/bath/bath_26.png"/>
+ </tile>
+ <tile id="26">
+  <image width="48" height="45" source="../imagens/bath/bath_27.png"/>
+ </tile>
+ <tile id="27">
+  <image width="48" height="45" source="../imagens/bath/bath_28.png"/>
+ </tile>
+ <tile id="28">
+  <image width="25" height="43" source="../imagens/bath/bath_29.png"/>
+ </tile>
+ <tile id="29">
+  <image width="44" height="48" source="../imagens/bath/bath_30.png"/>
+ </tile>
+ <tile id="30">
+  <image width="39" height="47" source="../imagens/bath/bath_31.png"/>
+ </tile>
+ <tile id="31">
+  <image width="16" height="13" source="../imagens/bath/bath_32.png"/>
+ </tile>
+ <tile id="32">
+  <image width="16" height="13" source="../imagens/bath/bath_33.png"/>
+ </tile>
+ <tile id="33">
+  <image width="9" height="23" source="../imagens/bath/bath_34.png"/>
+ </tile>
+ <tile id="34">
+  <image width="9" height="23" source="../imagens/bath/bath_35.png"/>
+ </tile>
+ <tile id="35">
+  <image width="24" height="24" source="../imagens/bath/bath_36.png"/>
+ </tile>
+ <tile id="36">
+  <image width="26" height="44" source="../imagens/bath/bath_37.png"/>
+ </tile>
+</tileset>
